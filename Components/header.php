226bb36b4941aec82d1a560f5cdc02dbcdb78a69
ViktorJGK <vjkalhovd@uia.no>
2024-10-08 13:02:49 +0200

    kan ikke se logg in og registrer når du er innlogget

--- Components/header.php
+++ Components/header.php
@@ -3,22 +3,31 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" href="../css/navbar.css">
     <title>Guest Profile</title>
 </head>
+
 <body>
     <div class="navbar">
         <a href="index.php">Hjem</a>
-        <a href="register.php">Registrer Bruker</a>
-        <a href="login.php">Logg Inn</a>
-        <a href="profile.php">Profil</a>
+        <?php if (!isset($_SESSION['user_id'])): ?>
+            <a href="register.php">Registrer Bruker</a>
+            <a href="login.php">Logg Inn</a>
+        <?php else: ?>
+            <a href="profile.php">Profil</a>
+        <?php endif; ?>
+
+
+
         <?php if (isset($_SESSION['user_id'])): ?>
             <form id="logout-form" action="../logout.php" method="POST" style="display:inline;">
-                <a href="../logout.php" type="submit" id="logout">Logg Ut</button> </a>
+                <a href="../logout.php" type="submit" id="logout">Logg Ut</a>
             </form>
         <?php endif; ?>
     </div>
     <br>
 </body>
+
 </html>
